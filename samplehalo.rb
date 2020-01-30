406a7a9d9e717318ce1cc130dbaab5e1a537199f
anthonytobar <37843535+anthonytobar@users.noreply.github.com>
2020-01-30 15:44:22 -0800

    Add files via upload

--- samplehalo.rb
+++ samplehalo.rb
@@ -1,50 +1,68 @@
 a ="C:/Users/anthony_tobar/Desktop/sonicpi/Man Screaming - CEEDAY Sound Effect (HD).wav"
 beat ="C:/Users/anthony_tobar/Desktop/sonicpi/Heartbeat Sound Effect Slow, Fast, Creepy, Irregular, Normal - Free Download I No Copyright.wav"
+b = "C:/Users/anthony_tobar/Desktop/sonicpi/behindyou.wav"
 x=1
 
 use_bpm 136
 use_synth :piano
 sample a
-sleep 5
+sleep 6
+live_loop :background_beat do
+  1.times do
+    sample beat
+    sleep 5
+  end
+  stop
+end
+
 4.times do
   x = x - 0.1
   sample a, rate: x
-  sleep 3
+  sleep 5
 end
 sleep 9
 live_loop :loop1 do
-  play :d
-  sleep 0.5
-  play :f
-  sleep 0.5
-  play :f
-  sleep 0.5
-  play :d
-  sleep 0.5
-  play :f
-  sleep 0.5
-  play :f
-  sleep 0.5
-  play :d
-  sleep  0.5
-  play :f
-  sleep 0.5
-  play :d
-  sleep 0.5
-  play :f
-  sleep 0.5
+  2.times do
+    play :d
+    sleep 0.5
+    play :f
+    sleep 0.5
+    play :f
+    sleep 0.5
+    play :d
+    sleep 0.5
+    play :f
+    sleep 0.5
+    play :f
+    sleep 0.5
+    play :d
+    sleep  0.5
+    play :f
+    sleep 0.5
+    play :d
+    sleep 0.5
+    play :f
+    sleep 0.5
+  end
+  stop
 end
 
 live_loop :loop2 do
-  play :f2, sustain: 10
-  sleep 5
-  play :f2, sustain: 10
-  sleep 5
-  play :f2, sustain: 10
-  sleep 5
+  1.times do
+    play :f2, sustain: 10
+    sleep 5
+    play :f2, sustain: 10
+    sleep 5
+    play :f2, sustain: 10
+    sleep 5
+  end
+  stop
 end
 
 live_loop :background_beat do
   sample beat
   sleep 5
 end
+
+sleep 15
+sample b
