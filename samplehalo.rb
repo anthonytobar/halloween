b5925ee410525de044e0f9ff5b24af0ed2e4db18
anthonytobar <37843535+anthonytobar@users.noreply.github.com>
2020-02-05 14:40:48 -0800

    Add files via upload

--- samplehalo.rb
+++ samplehalo.rb
@@ -1,16 +1,17 @@
-a ="C:/Users/anthony_tobar/Desktop/sonicpi/Man Screaming - CEEDAY Sound Effect (HD).wav"
-beat ="C:/Users/anthony_tobar/Desktop/sonicpi/Heartbeat Sound Effect Slow, Fast, Creepy, Irregular, Normal - Free Download I No Copyright.wav"
+scream ="C:/Users/anthony_tobar/Desktop/sonicpi/manscream.wav"
+heart ="C:/Users/anthony_tobar/Desktop/sonicpi/heart.wav"
 beat2 ="C:/Users/anthony_tobar/Desktop/sonicpi/freddylullaby.wav"
 b = "C:/Users/anthony_tobar/Desktop/sonicpi/behindyou.wav"
 x=1
+g=0.5
 
 use_bpm 136
 use_synth :piano
-sample a
+sample scream
 sleep 6
 live_loop :background_beat do
   1.times do
-    sample beat
+    sample heart
     sleep 5
   end
   stop
@@ -18,40 +19,38 @@ end
 
 4.times do
   x = x - 0.1
-  sample a, rate: x
+  sample scream, rate: x
   sleep 5
 end
 sleep 9
 live_loop :loop1 do
   2.times do
     play :d
-    sleep 0.5
+    sleep g
     play :f
-    sleep 0.5
+    sleep g
     play :f
-    sleep 0.5
+    sleep g
     play :d
-    sleep 0.5
+    sleep g
     play :f
-    sleep 0.5
+    sleep g
     play :f
-    sleep 0.5
+    sleep g
     play :d
-    sleep  0.5
+    sleep g
     play :f
-    sleep 0.5
+    sleep g
     play :d
-    sleep 0.5
+    sleep g
     play :f
-    sleep 0.5
+    sleep g
   end
   stop
 end
 
 live_loop :loop2 do
   1.times do
-    play :f2, sustain: 10
-    sleep 5
     play :f2, sustain: 10
     sleep 5
     play :f2, sustain: 10
